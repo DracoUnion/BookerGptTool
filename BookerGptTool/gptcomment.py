4c4a1b8065c8203fa26055ec48d56a5165948584
wizardforcel <wizard.z@qq.com>
2024-01-04 17:42:03 +0800

    2024-01-04 17:42:03

---
+++ BookerGptTool/gptcomment.py
@@ -0,0 +1,153 @@
+import openai
+import httpx
+import os
+import traceback
+import yaml
+import argparse
+from os import path
+import json
+import random
+import copy
+import re
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+
+__version__ = '2023.12.25.0'
+
+DFT_PROMPT = '''
+假设你是一位资深的程序员，请你参照示例为给定代码的每个语句添加注释，解释它们的作用。
+
+示例：
+
+```
+# 根据 ZIP 文件名读取内容，返回其中文件名到数据的字典
+def read_zip(fname):
+    # 根据 ZIP 文件名读取其二进制，封装成字节流
+    bio = BytesIO(open(fname, 'rb').read())
+    使用字节流里面内容创建 ZIP 对象
+    zip = zipfile.ZipFile(bio, 'r')
+    遍历 ZIP 对象所包含文件的文件名，读取文件数据，组成文件名到数据的字典
+    fdict = {n:zip.read(n) for n in zip.namelist()}
+    # 关闭 ZIP 对象
+    zip.close()
+    # 返回结果字典
+    return fdict
+```
+
+需要注释的代码：
+
+```
+{code}
+```
+'''
+
+def call_openai_retry(code, prompt, model_name, retry=10):
+    for i in range(retry):
+        try:
+            ques = prompt.replace('{code}', code)
+            print(f'ques: {json.dumps(ques, ensure_ascii=False)}')
+            client = openai.OpenAI(
+                base_url=openai.host,
+                api_key=openai.api_key,
+                http_client=httpx.Client(
+                    proxies=openai.proxy,
+                    transport=httpx.HTTPTransport(local_address="0.0.0.0"),
+                )
+            )
+            ans = client.chat.completions.create(
+                messages=[{
+                    "role": "user",
+                    "content": ques,
+                }],
+                model=model_name,
+                temperature=0,
+            ).choices[0].message.content
+            ans = re.sub(r'\A```\w*\n', '', ans)
+            ans = re.sub(r'\n```\Z', '', ans)
+            print(f'ans: {json.dumps(ans, ensure_ascii=False)}')
+            return ans
+        except Exception as ex:
+            print(f'OpenAI retry {i+1}: {str(ex)}')
+            if i == retry - 1: raise ex
+
+def chunk_code(lines, limit=20):
+    if isinstance(lines, str):
+        lines = lines.split('\n')
+        
+    lines = [l for l in lines if len(l) <= 200]
+    blocks = []
+    for i in range(0, len(lines), limit):
+        blocks.append(lines[i:i+limit])
+    
+    return blocks
+
+def process_dir(args):
+    dir = args.fname
+    pool = ThreadPoolExecutor(args.threads)
+    hdls = []
+    for base, _, fnames in os.walk(dir):
+        for f in fnames:
+            args = copy.deepcopy(args)
+            args.fname = path.join(base, f)
+            h = pool.submit(process_file_safe, args)
+            hdls.append(h)
+    for h in hdls: h.result()
+
+def process_file_safe(args):
+    try:
+        process_file(args)
+    except:
+        traceback.print_exc()
+
+def process_file(args):
+    fname = args.fname
+    ext = extname(fname)
+    if ext not in ['c', 'h', 'cpp', 'cxx', 'java', 'cs', 'php', 'go', 'js', 'ts', 'py']:
+        print(f'{fname} 代码类型不支持')
+        return
+    ofname = fname + '.md'
+    if path.isfile(ofname):
+        print(f'{fname} 已存在')
+        return
+    print(fname)
+    code = open(fname, encoding='utf8').read()
+    blocks = chunk_code(code, args.limit)
+    parts = []
+    for b in blocks:
+        code = '\n'.join(b)
+        part = call_openai_retry(code, args.prompt, args.model, args.retry)
+        parts.append(part)
+    comment = '```\n' + '\n'.join(parts) + '\n```'
+    print(f'==={fname}===\n{comment}')
+    res = f'# `{fname}`\n\n{comment}'
+    open(ofname, 'w', encoding='utf8').write(res)
+    
+
+def extname(name):
+    m = re.search(r'\.(\w+)$', name)
+    return m.group(1) if m else ''
+
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fname', help='file or dir name')
+    parser.add_argument('-p', '--prompt', default=DFT_PROMPT, help='prompt for code comment')
+    parser.add_argument("-P", "--proxy", help="proxy")
+    parser.add_argument("-m", "--model", default='gpt-3.5-turbo-1106', help="model name")
+    parser.add_argument("-k", "--key", default=os.environ.get('OPENAI_API_KEY', ''), help="OpenAI API key")
+    parser.add_argument("-r", "--retry", type=int, default=10, help="times of retry")
+    parser.add_argument("-H", "--host", help="api host")
+    parser.add_argument("-t", "--threads", type=int, default=8, help="thread num")
+    parser.add_argument("-l", "--limit", type=int, default=20, help="lines limit")
+    args = parser.parse_args()
+    
+    openai.api_key = args.key
+    openai.proxy = args.proxy
+    openai.host = args.host
+ 
+    if path.isdir(args.fname):
+        process_dir(args)
+    else:
+        process_file(args)
+        
+if __name__ == '__main__': main()
---
+++ BookerGptTool/gpttrans.py
@@ -0,0 +1,196 @@
+import openai
+import httpx
+import os
+import traceback
+import yaml
+import argparse
+from os import path
+import json
+import random
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+
+__version__ = '2023.12.11.0'
+
+DFT_PROMPT = '请把以下文本按行翻译成中文，不要输出原文：\n\n{en}'
+
+def shuffle_group(g):
+    count = len(g['ids'])
+    idcs = list(range(count))
+    random.shuffle(idcs)
+    g['ids'] = [g['ids'][i] for i in idcs]
+    g['ens'] = [g['ens'][i] for i in idcs]
+
+def trans_openai_retry(en, prompt, model_name, retry=10):
+    for i in range(retry):
+        try:
+            ques = prompt.replace('{en}', en)
+            print(f'ques: {json.dumps(ques, ensure_ascii=False)}')
+            client = openai.OpenAI(
+                base_url=openai.host,
+                api_key=openai.api_key,
+                http_client=httpx.Client(
+                    proxies=openai.proxy,
+                    transport=httpx.HTTPTransport(local_address="0.0.0.0"),
+                )
+            )
+            ans = client.chat.completions.create(
+                messages=[{
+                    "role": "user",
+                    "content": ques,
+                }],
+                model=model_name,
+                temperature=0,
+            ).choices[0].message.content
+            print(f'ans: {json.dumps(ans, ensure_ascii=False)}')
+            return ans
+        except Exception as ex:
+            print(f'OpenAI retry {i+1}: {str(ex)}')
+            if i == retry - 1: raise ex
+    
+def group_totrans(totrans, limit):
+    groups = [] # [{ids: [str], ens: [str]}]
+    for it in totrans:
+        if not it.get('en') or it.get('zh'):
+            continue
+        if it.get('type') in ['TYPE_PRE']:
+            continue
+        if len(groups) == 0:
+            groups.append({
+                'ids': [it['id']],
+                'ens': [it['en']]
+            })
+        else:
+            total = len('\n'.join(groups[-1]['ens']))
+            if total + len(it['en']) <= limit:
+               groups[-1]['ids'].append(it['id']) 
+               groups[-1]['ens'].append(it['en']) 
+            else:
+                groups.append({
+                    'ids': [it['id']],
+                    'ens': [it['en']]
+                })
+    return groups
+
+def preproc_totrans(totrans):
+    for i, it in enumerate(totrans):
+        if not it.get('id'):
+            it['id'] = f'totrans-{i}'
+        if it.get('en'):
+            it['en'] = it['en'].replace('\n', '')
+        if it['type'] == 'TYPE_PRE':
+            it['zh'] = it.get('en', '')
+
+def tr_trans(g, args, totrans_id_map, write_callback=None):
+    for i in range(args.retry):
+        try:
+            shuffle_group(g)    
+            en = '\n'.join(g['ens'])
+            ans = trans_openai_retry(en, args.prompt, args.model, args.retry)
+            zhs = [zh for zh in ans.split('\n') if zh]
+            assert len(g['ids']) == len(zhs)
+            break
+        except Exception as ex:
+            print(f'en-zh match retry {i+1}')
+            if i == args.retry - 1: raise ex
+    for id, zh in zip(g['ids'], zhs):
+        totrans_id_map.get(id, {})['zh'] = zh
+    # 及时保存已翻译文本
+    if write_callback: write_callback()
+
+def tr_trans_safe(*args, **kw):
+    try:
+        tr_trans(*args, **kw)
+    except:
+        traceback.print_exc()
+
+def trans_one(totrans, args, write_callback=None):
+    # totrans: [{id?: str, en?: str, zh?: str, type: str, ...}]
+    preproc_totrans(totrans)
+    groups = group_totrans(totrans, args.limit)
+    totrans_id_map = {it['id']:it for it in totrans}
+    
+    pool = ThreadPoolExecutor(args.threads)
+    hdls = []
+    for g in groups:
+        h = pool.submit(
+            tr_trans_safe, 
+            g, args, totrans_id_map, 
+            write_callback,
+        )
+        hdls.append(h)
+    for h in hdls: h.result()
+    return totrans
+
+
+file_lock = Lock()
+
+def write_callback(fname, totrans):
+    with file_lock:
+        open(fname, 'w', encoding='utf8') \
+            .write(yaml.safe_dump(totrans, allow_unicode=True))
+
+def trans_handle(args):
+    print(args)
+    openai.api_key = args.key
+    openai.proxy = args.proxy
+    openai.host = args.host
+    fname = args.fname
+    if path.isfile(fname):
+        fnames = [fname]
+    elif path.isdir(fname):
+        fnames = [
+            path.join(fname, f) 
+            for f in os.listdir(fname) 
+            if f.endswith('.yaml')
+        ]
+    else:
+        raise Exception('请提供 YAML 文件或其目录')
+        
+    for f in fnames:
+        print(f)
+        totrans = yaml.safe_load(open(f, encoding='utf8').read())
+        trans_one(totrans, args, lambda: write_callback(f, totrans))
+        
+    
+def test_handle(args):
+    print(args)
+    openai.api_key = args.key
+    openai.proxy = args.proxy
+    openai.host = args.host
+    ans = trans_openai_retry(args.en, args.prompt, args.model)
+    
+     
+def main():
+    parser = argparse.ArgumentParser(prog="GPTTrans", description="ChatGPT training and translation tool for GLM", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-v", "--version", action="version", version=f"BookerMarkdownTool version: {__version__}")
+    parser.set_defaults(func=lambda x: parser.print_help())
+    subparsers = parser.add_subparsers()
+    
+    trans_parser = subparsers.add_parser("trans", help="translate YAML files")
+    trans_parser.add_argument("fname", help="yaml file name of dir")
+    trans_parser.add_argument("-p", "--prompt", default=DFT_PROMPT, help="prompt for trans")
+    trans_parser.add_argument("-P", "--proxy", help="proxy")
+    trans_parser.add_argument("-m", "--model", default='gpt-3.5-turbo', help="model name")
+    trans_parser.add_argument("-l", "--limit", type=int, default=4000, help="max token limit")
+    trans_parser.add_argument("-k", "--key", default=os.environ.get('OPENAI_API_KEY', ''), help="OpenAI API key")
+    trans_parser.add_argument("-r", "--retry", type=int, default=10, help="times of retry")
+    trans_parser.add_argument("-H", "--host", help="api host")
+    trans_parser.add_argument("-t", "--threads", type=int, default=8, help="thread num")
+    trans_parser.set_defaults(func=trans_handle)
+    
+    test_parser = subparsers.add_parser("test", help="testing model with YAML files")
+    test_parser.add_argument("en", help="en text")
+    test_parser.add_argument("-p", "--prompt", default=DFT_PROMPT, help="prompt for trans")
+    test_parser.add_argument("-P", "--proxy", help="proxy")
+    test_parser.add_argument("-m", "--model", default='gpt-3.5-turbo', help="model name")
+    test_parser.add_argument("-l", "--limit", type=int, default=4000, help="max token limit")
+    test_parser.add_argument("-k", "--key", default=os.environ.get('OPENAI_API_KEY', ''), help="OpenAI API key")
+    test_parser.add_argument("-r", "--retry", type=int, default=10, help="times of retry")
+    test_parser.add_argument("-H", "--host", help="api host")
+    test_parser.set_defaults(func=test_handle)
+    
+    args = parser.parse_args()
+    args.func(args)
+
+if __name__ == '__main__': main()
