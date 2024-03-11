043948b1e08214197b775851c8af27beafbe3d7b
wizardforcel <wizard.z@qq.com>
2024-03-11 14:19:59 +0800

    2024-03-11 14:19:59

--- BookerGptTool/arxiv.py
+++ BookerGptTool/arxiv.py
@@ -1,2 +1,50 @@
 from .util import *
+import requests
+import zipfile
+from io import BytesIO
 
+DFT_ARXIV_PROMPT = '''
+假设你是一个高级科研人员和人工智能专家，请按照给定大纲总结给定论文：
+
+## 大纲
+
+1.  创新点
+2.  重要性
+3.  相关工作
+    1.   {工作1}
+    2.   {工作2}
+    3.   ...
+4.  所使用的模型
+    1.  模型名称
+    2.  模型结构简介
+5.  实验设置
+    1.  数据集
+    2.  任务类型
+    3.  评价指标
+6.  实验结果
+    1.  本模型在 {任务} 和 {数据集} 上的 {指标} 是 {xxx}，对比 {其它模型} 的 {yyy} 提升了 {zzz}。
+7.  未来工作
+
+## 论文
+
+{text}
+'''
+
+def sum_arxiv(args):
+    print(args)
+    aid = args.arxiv
+    set_openai_props(args.key, args.proxy, args.host)
+    url = f'https://arxiv.org/src/{aid}'
+    data = requests.get(url).content
+    zip = zipfile.ZipFile(BytesIO(data))
+    tex_fnames = [
+        n for n in zip.namelist()
+        if n.endswith('.tex')
+    ]
+    if not tex_fnames:
+        print('找不到 TEX 文件')
+        return
+    tex = zip.read(tex_fnames[0])[:args.limit]
+    ques = args.prompt.replace('{text}', tex)
+    ans = call_openai_retry(ques, args.model, args.retry)
+    open(f'{aid}.txt', 'w', encoding='utf8').write(ans)
