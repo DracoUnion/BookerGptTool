bdcd3fab3a778bb349a3cd1b1b0bd9971d0c8f52
wizardforcel <562826179@qq.com>
2024-03-12 10:26:24 +0800

    2024-03-12 10:26:24

---
+++ BookerGptTool/sum.py
@@ -0,0 +1,23 @@
+from .util import *
+import re
+
+DFT_SUM_PMT = '''
+请分段总结以下内容：
+
+{text}
+'''
+
+def sum_text(args):
+    print(args)
+    ext = extname(args.fname)
+    if ext not in ['md', 'srt']:
+       print('请提供 MD 或者 SRT 文件')
+       return
+    cont = open(args.fname, encoding='utf8').read()[:args.limit]
+    ques = args.prompt.replace('{text}', cont)
+    ans = call_openai_retry(ques, args.model, args.retry)
+    ofname = re.sub(r'\.\w+$', '', args.fname) + '_sum.md'
+    if ext == 'md':
+        title, _ = get_md_title(cont)
+        if title: ans = f'# {title}\n\n{ans}'
+    open(ofname, 'w', encoding='utf8').write(ans)
