a3d9c0143bcb0465ad322675b95ccf4a1fcc7dfb
wizardforcel <wizard.z@qq.com>
2024-07-31 16:15:31 +0800

    2024-07-31 16:15:31

--- BookerGptTool/call.py
+++ BookerGptTool/call.py
@@ -1,6 +1,7 @@
 from .util import *
 
 def call_handle(args):
+    print(args)
     set_openai_props(args.key, args.proxy, args.host)
     ans = call_chatgpt_retry(args.ques, args.model, args.temp, args.retry)
     print(ans)
