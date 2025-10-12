2b2ecf1ff4067c3df21fd0eabe33343ba703325c
wizardforcel <562826179@qq.com>
2025-10-12 10:45:07 +0800

    2025-10-12 10:45:07

---
+++ BookerGptTool/xhs.py
@@ -0,0 +1,69 @@
+import openai
+import httpx
+import os
+import traceback
+import yaml
+import argparse
+import copy
+from os import path
+import json
+import random
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+import functools
+from .util import *
+
+XHS_PMT = '''
+假设你是一位小红书资深博主，请参考下面的素材，生成一篇吸引人的小红书笔记。
+
+## 注意
+
+1.  只需要输出笔记，不需要输出任何其它东西。
+
+## 素材
+
+{text}
+'''
+
+def gen_xhs_single(args):
+    cont = open(args.fname, encoding='utf8').read()
+    ques = XHS_PMT.replace('{text}', cont)
+    ans = call_chatgpt_retry(ques, args.model, args.temp, args.retry)
+    ofname = args.fname[:-4] + '_xhs.txt'
+    open(ofname, 'w', encoding='utf8').write(ans)
+    print(ofname)
+
+def gen_xhs_single_safe(args):
+    try:
+        gen_xhs_single(args)
+    except:
+        traceback.print_exc()
+
+def gen_xhs(args):
+    print(args)
+    set_openai_props(args)
+
+    if path.isfile(args.fname):
+        fnames = [args.fname]
+    else:
+        fnames = [
+            path.join(args.fname, f) 
+            for f in os.listdir(args.fname)
+        ]
+    fnames = [f for f in fnames if extname(f) in 'txt']
+    if not fnames:
+        print('请提供 TXT 文件')
+        return
+
+    pool = ThreadPoolExecutor(args.threads)
+    hdls = []
+    for f in fnames:
+        args = copy.deepcopy(args)
+        args.fname = f
+        h = pool.submit(gen_xhs_single_safe, args)
+        hdls.append(h)
+        if len(hdls) > args.threads:
+            for h in hdls: h.result()
+
+    for h in hdls: h.result()
+    
