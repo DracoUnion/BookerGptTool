30d634768077294f8a3574474c98f65d6db568db
布客飞龙 <562826179@qq.com>
2026-04-24 20:03:17 +0800

    Update trans_epub.py

--- BookerGptTool/trans_epub.py
+++ BookerGptTool/trans_epub.py
@@ -1,1 +1,25 @@
+TRANS_TITLE_PMT = '''
+你是一个高级翻译，请参考示例，翻译指定的图书标题到中文。注意只需要输出翻译，不要输出其他任何东西。
 
+## 示例
+
++   “with”不翻译，比如“deep learning with python”翻译为“python 深度学习”
++   “beginners guide”翻译为“初学者指南”
++   “cookbook”翻译为“秘籍”
++   “build xxx”翻译为“xxx构建指南”
++   “xxx in action”翻译为“xxx 实战”
++   “hands on xxx”翻译为“xxx 实用指南”
++   “practice xxx”翻译为“xxx 实践指南”
++   “unlock xxx”翻译为“xxx 解锁指南”
++   “xxx by example”翻译为“xxx 示例”
++   “pro xxx”翻译为“xxx 高级指南”
++   “xxx for yyy”翻译为“yyy 的 xxx”
++   “using xxx”翻译为“xxx 使用指南”
++   “introduction to xxx”或者“beginning xxx”翻译为“xxx 入门指南”
++   “quick start guide”翻译为“快速启动指南”
++   “playbook”翻译为“攻略书”
+
+## 要翻译的标题
+
+{text}
+'''
