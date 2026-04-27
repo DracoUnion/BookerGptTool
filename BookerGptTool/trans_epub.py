3a759af9c1612d9e8073c4f51eba549d45177f33
布客飞龙 <562826179@qq.com>
2026-04-27 10:29:51 +0800

    Add fmt_zh import to trans_epub.py

--- BookerGptTool/trans_epub.py
+++ BookerGptTool/trans_epub.py
@@ -8,7 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
 import json_repair as json
 from imgyaso.quant import pngquant
-from .util import call_chatgpt_retry, set_openai_props, to_kebab, read_zip, is_pic, tomd, get_md_title, epub2html_pandoc
+from .util import call_chatgpt_retry, set_openai_props, to_kebab, read_zip, is_pic, tomd, get_md_title, epub2html_pandoc, fmt_zh
 from .md2skill_chunker import chunk_markdown
 
 TRANS_BODY_PMT = '''
