8333250007ac1f2407d24ed3b4d84c243e770737
wizardforcel <562826179@qq.com>
2026-07-09 13:36:32 +0800

    2026-07-09 13:36:32

--- BookerGptTool/util.py
+++ BookerGptTool/util.py
@@ -229,7 +229,7 @@ def call_llm(
 ):
     # 改变指令符号的形式，避免模型出错
     msgs = repl_ins_token(msgs)
-    print(f'ques: {get_msgs_text(msgs)}')
+    print(f'ques: {json.dumps(get_msgs_text(msgs), ensure_ascii=False)}')
     client = openai.OpenAI(
         base_url=openai.base_url,
         api_key=openai.api_key,
@@ -269,7 +269,7 @@ def call_llm(
     # 还原指令格式
     ans = re.sub(r'</([\w\-\.]+)/>', r'<|\1|>', ans)
     ans = re.sub(r'<think>[\s\S]+?</think>', '', ans)
-    print(f'ans: {ans}')
+    print(f'ans: {json.dumps(ans, ensure_ascii=False)}')
     return ans
 
 def set_openai_props(args):
