b463b0e645d0384e4118febfe093a3c7c2be73d4
wizardforcel <562826179@qq.com>
2026-07-31 20:16:58 +0800

    2026-07-31 20:16:58

--- BookerGptTool/__main__.py
+++ BookerGptTool/__main__.py
@@ -206,6 +206,7 @@ def main():
     forward_parser.add_argument("fname", help="yaml file name containing keys")
     forward_parser.add_argument("-lh", "--listen-host", type=str, default='localhost', help="")
     forward_parser.add_argument("-lp", "--listen-port", type=int, default=5000, help="")
+    forward_parser.add_argument("-w", "--waitress", action='store_true', help="")
     forward_parser.add_argument("-t", "--threads", type=int, default=8, help="")
     forward_parser.add_argument("-D", "--debug", action='store_true', help="")
     forward_parser.set_defaults(func=forward)
