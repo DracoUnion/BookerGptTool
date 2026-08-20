e4ed6730cd53808afaa2c94f9bb50121c51dee3d
wizardforcel <562826179@qq.com>
2026-08-20 12:12:24 +0800

    2026-08-20 12:12:24

--- BookerGptTool/__main__.py
+++ BookerGptTool/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import os
+import multiprocessing as mp
 from . import __version__
 from .trans import *
 from .fin_report import *
@@ -27,6 +28,8 @@ from .novel_anls import *
 from .xhs_img import register_xhs_img
 
 def main():
+    mp.set_start_method('spawn')
+
     openai_key = os.environ.get('OPENAI_API_KEY')
     openai_url = os.environ.get('OPENAI_BASE_URL')
     openai_model = os.environ.get('OPENAI_CHAT_MODEL', 'gpt-3.5-turbo')
