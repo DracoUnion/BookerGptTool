1b1f408bfe94a743e07223022158633522f7985a
wizardforcel <562826179@qq.com>
2026-08-28 14:57:02 +0800

    2026-08-28 14:57:02

--- BookerGptTool/__main__.py
+++ BookerGptTool/__main__.py
@@ -78,7 +78,7 @@ def main():
     code2book_parser = subparsers.add_parser("code2book", help="code to book")
     code2book_parser.add_argument('dir', help='proj dir name')
     code2book_parser.add_argument("-t", "--threads", type=int, default=8, help="thread num")
-    code2book_parser.add_argument("-c", "--check", type=int, default=0, help="check times")
+    code2book_parser.add_argument("-c", "--check", type=int, default=3, help="check times")
     code2book_parser.add_argument("-l", "--chapter-limit", type=int, default=20, help="chapter limit")
     code2book_parser.add_argument("-D", "--debug", action='store_true', help="debug mode")
     code2book_parser.set_defaults(func=code2book)
